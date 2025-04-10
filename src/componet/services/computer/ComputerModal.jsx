8f839f4c7b706f23478fe2d7c4a7cbfcb44f1011
Prince Sharma <hit.princeraj@gmail.com>
2025-04-10 23:08:17 +0530

    comment code

--- src/componet/services/computer/ComputerModal.jsx
+++ src/componet/services/computer/ComputerModal.jsx
@@ -1,16 +1,17 @@
 
-import { useGLTF } from '@react-three/drei'
-export function computerModal(props) {
-  const { nodes, materials } = useGLTF('/computerModal.glb')
-  return (
-    <group {...props} dispose={null}>
-      <group position={[0.121, 0.007, 0]}>
-        <mesh geometry={nodes.Object_6.geometry} material={materials.MacBookPro} />
-        <mesh geometry={nodes.Object_8.geometry} material={materials.MacBookPro} />
-      </group>
-      <mesh geometry={nodes.Object_4.geometry} material={materials.MacBookPro} />
-    </group>
-  )
-}
+// import { useGLTF } from '@react-three/drei'
+// export function computerModal(props) {
+//   const { nodes, materials } = useGLTF('/computerModal.glb')
+//   return (
+//     <group {...props} dispose={null}>
+//       <group position={[0.121, 0.007, 0]}>
+//         <mesh geometry={nodes.Object_6.geometry} material={materials.MacBookPro} />
+//         <mesh geometry={nodes.Object_8.geometry} material={materials.MacBookPro} />
+//       </group>
+//       <mesh geometry={nodes.Object_4.geometry} material={materials.MacBookPro} />
+//     </group>
+//   )
+// }
 
-useGLTF.preload('/computerModal.glb');
+// useGLTF.preload('/computerModal.glb');
+// 
